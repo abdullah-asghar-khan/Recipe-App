16a31b81cc6467368c54f5903c276ba1497fe9f9
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-07 20:00:56 +0100

    Fix errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,27 +1,26 @@
 class FoodsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @foods = Food.all
   end
-  
+
   def new
     @foods = Food.new
   end
-  
-  def create  
+
+  def create
     @new_food = Food.new(name: params[:name],
-                        measurment_unit: params[:measurment_unit],
-                        price: params[:unit_price],
-                        uantity: params[:quantity],
-                        user_id: current_user.id)
- 
+                         measurment_unit: params[:measurment_unit],
+                         price: params[:unit_price],
+                         quantity: params[:quantity],
+                         user_id: current_user.id)
 
     @new_food.save
 
     redirect_to foods_path
   end
-  
+
   def destroy
     Food.find(params[:id]).destroy
     redirect_to foods_path
