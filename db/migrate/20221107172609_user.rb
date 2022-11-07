90c750549f739f48537caf8ad04813cf87abb295
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-08 03:11:43 +0500

    Added mail catcher to the sign and sign up config

--- db/migrate/20221107172609_user.rb
+++ db/migrate/20221107172609_user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
+      t.string :name
 
       t.timestamps
     end
