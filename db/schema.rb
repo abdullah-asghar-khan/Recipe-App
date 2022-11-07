dd18d12e7cd9f0c1dc45b80c12d4163c7f1b3879
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-08 03:25:42 +0500

    Update some changes, revert back to locahost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_180325) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_220402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,12 +25,36 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_180325) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+  create_table "recipes", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.integer "preparation_time", default: 0
+    t.integer "cooking_time", default: 0
+    t.text "description", default: "", null: false
+    t.boolean "public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end
