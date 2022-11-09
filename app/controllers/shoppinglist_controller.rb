6202581e9e78942e870ca550cc9b19d14c099662
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-09 15:50:46 +0500

    Still trying to get total price but return empty

--- app/controllers/shoppinglist_controller.rb
+++ app/controllers/shoppinglist_controller.rb
@@ -5,12 +5,20 @@ class ShoppinglistController < ApplicationController
     @user = current_user
     @foods = @user.foods
     @recipes = Recipe.where(user_id: current_user)
+    @recipe_food = RecipeFood.all
     @total_price = 0
+    #@total_price = current_user.sum(@recipe.recipe_food.includes([:food]))
 
-    @quantity = 0
+    
 
-    @foods.each do |food|
-      @quantity += RecipeFood.joins(:food).where(food_id: food.id).sum('quantity') * food.price
+    #@quantity = 0
+
+    # @foods.each do |food|
+    #   @quantity += RecipeFood.joins(:food).where(food_id: food.id).sum('quantity') * food.price
+    # end
+
+    @recipe_food.each do |fd|
+      @total_price += fd.quantity * fd.food.price
     end
   end
 end
