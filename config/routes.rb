0c58de5533183b615a53e3bbe6e36c48b68ce3c3
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-09 21:08:42 +0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,10 @@
 Rails.application.routes.draw do
-
-  # root "recipes#public_recipes"
-
-  # devise_for :users, skip: [:sessions]
-  # as :user do
-  #   get 'login', to: 'devise/sessions#new', as: :new_user_session
-  #   post 'register', to: 'devise/sessions#create', as: :user_session
-  #   get 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
-  # end
-  
-  # get 'public_recipes', to: 'recipes#public_recipes'
-  # resources :users, only: [:index, :show, :new, :create, :destroy] 
-
-  # resources :recipes, only: [:index, :show, :new, :create, :destroy ] do
-  #   resources :recipe_foods, only: [:new, :create, :edit, :destroy]
-  # end
-
-  # resources :foods, only: [:index, :new, :create, :destroy ]   
-    
   resources :foods, only: [:index, :new, :create, :destroy ]
-  resources :recipes, only: [:index, :new, :create, :destroy ]
-  
+  #resources :recipes, only: [:index, :new, :create, :destroy ]
+  resources :recipes do
+    resources :recipe_foods
+    resources :shoppinglist
+  end
   get '/shoppinglist', to: 'shoppinglist#index' 
   
   devise_for :users
