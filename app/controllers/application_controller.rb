49817f60a474b4318959996e6b14e54d4067227f
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-10 17:14:35 +0100

    Fix rubocop linter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,3 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 end
-  
