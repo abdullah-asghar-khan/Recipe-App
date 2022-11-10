587bc1ebb546f5399435069229d436979ec082a5
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-10 08:54:14 +0100

    Fix routing error

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def destroy; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,4 @@
 Rails.application.routes.draw do
-
-  # root "recipes#public_recipes"
-
-  # devise_for :users, skip: [:sessions]
-  # as :user do
-  #   get 'login', to: 'devise/sessions#new', as: :new_user_session
-  #   post 'register', to: 'devise/sessions#create', as: :user_session
-  #   get 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
-  # end
-  
-  # get 'public_recipes', to: 'recipes#public_recipes'
-  # resources :users, only: [:index, :show, :new, :create, :destroy] 
-
-  # resources :recipes, only: [:index, :show, :new, :create, :destroy ] do
-  #   resources :recipe_foods, only: [:new, :create, :edit, :destroy]
-  # end
-
-  # resources :foods, only: [:index, :new, :create, :destroy ]   
   
   resources :recipes, only: [:index, :new, :create, :destroy ]
   resources :foods, only: [:index, :new, :create, :destroy ]
@@ -28,5 +10,4 @@ Rails.application.routes.draw do
  end
 
   root to: "home#index"
-
 end
