01a1b70a58e825e02c9a324aed2bfd7d4f6bbacd
Abdullah2213565 <dulakhan024@gmail.com>
2022-11-10 11:31:10 +0100

    Edit database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 end
+  
