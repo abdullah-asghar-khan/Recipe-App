b2bbbbed0d3273fbf5f69877cdc61cbe7910fdd0
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-11 14:16:21 +0500

    Updated tests

--- spec/requests/food_spec.rb
+++ spec/requests/food_spec.rb
@@ -1,39 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe 'Foods', type: :request do
-#   @user = User.create!(name: 'john', email: 'test@abc.com', password: '12345678', password_confirmation: '12345678')
-#   #@user.sign_in_count = 1
-#   @user.save
-
-#   context 'GET /index' do
-#     before(:all) do
-#       get '/foods'
-#     end
-
-#     it 'returns http success' do
-#       expect(response).to have_http_status(:ok)
-#     end
-
-#     it 'Correct template was rendered.' do
-#       expect(response).to render_template(:index)
-#     end
-#   end
-
-#   context 'GET /new' do
-#     before(:all) do
-#       get '/foods/new'
-#     end
-
-#     it 'returns http success' do
-#       expect(response).to have_http_status(:ok)
-#     end
-
-#     it 'Correct template was rendered.' do
-#       expect(response).to render_template(:new)
-#     end
-#   end
-# end
-
 require 'rails_helper'
 
 RSpec.describe 'Foods', type: :request do
@@ -44,7 +8,6 @@ RSpec.describe 'Foods', type: :request do
 
   describe 'GET /index' do
     before do
-      #sign_in user
       get foods_path
     end
 
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,89 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe '/recipes', type: :request do
-  context 'GET /index' do
-    before(:each) do
-      get '/recipes'
+RSpec.describe 'RecipeFood', type: :request do
+  include Devise::Test::IntegrationHelpers
+  let(:user) { User.create(name: 'Kolly', email: 'kolly@mail.com', password: 'password') }
+  let(:food) { user.foods.create(name: 'apple', measurement_Unit: 'kg', price: 4) }
+  let(:recipe) do
+    user.recipes.create(name: 'Pizza', cooking_time: 1, preparation_time: 2, description: 'hgghghghghh', public: false)
+  end
+  describe 'GET /index' do
+    before do
+      #sign_in user
+      get new_recipe_recipe_food_path(recipe)
     end
 
-    it 'Correct template was rendered.' do
-      expect(response).to render_template(:index)
-    end
-
-    it 'Response status was correct.' do
-      expect(response).to have_http_status(:ok)
+    it 'response to html' do
+      expect(response.content_type).to include 'text/html'
     end
   end
-
-  # describe 'GET /show' do
-  #   it 'renders a successful response' do
-  #     recipe = Recipe.create! valid_attributes
-  #     get recipe_url(recipe)
-  #     expect(response).to be_successful
-  #   end
-  # end
-
-  # context 'Show' do
-  #   before(:each) do
-  #     get '/recipes/1'
-  #   end
-  #   it 'Correct template was rendered.' do
-  #     expect(response).to render_template(:show)
-  #   end
-  #   it 'Response status was correct.' do
-  #     expect(response).to have_http_status(:ok)
-  #   end
-  # end
-
-  # describe 'GET /new' do
-  #   it 'renders a successful response' do
-  #     get new_recipe_url
-  #     expect(response).to be_successful
-  #   end
-  # end
-
-  # describe 'POST /create' do
-  #   context 'with valid parameters' do
-  #     it 'creates a new Recipe' do
-  #       expect do
-  #         post recipes_url, params: { recipe: valid_attributes }
-  #       end.to change(Recipe, :count).by(1)
-  #     end
-
-  #     it 'redirects to the created recipe' do
-  #       post recipes_url, params: { recipe: valid_attributes }
-  #       expect(response).to redirect_to(recipe_url(Recipe.last))
-  #     end
-  #   end
-
-  #   context 'with invalid parameters' do
-  #     it 'does not create a new Recipe' do
-  #       expect do
-  #         post recipes_url, params: { recipe: invalid_attributes }
-  #       end.to change(Recipe, :count).by(0)
-  #     end
-
-  #     it "renders a successful response (i.e. to display the 'new' template)" do
-  #       post recipes_url, params: { recipe: invalid_attributes }
-  #       expect(response).to be_successful
-  #     end
-  #   end
-  # end
-
-  # describe 'DELETE /destroy' do
-  #   before(:each) do
-  #     recipe = Recipe.create! valid_attributes
-  #   end
-
-  #   it 'destroys the requested recipe' do
-  #     expect do
-  #       delete recipe_url(recipe)
-  #     end.to change(Recipe, :count).by(-1)
-  #   end
-
-  #   it 'redirects to the recipes list' do
-  #     delete recipe_url(recipe)
-  #     expect(response).to redirect_to(recipes_url)
-  #   end
-  # end
 end
